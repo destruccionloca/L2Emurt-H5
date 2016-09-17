bc53dd31ce24e0ffa093cbe359db068e3ed6a6c3
trick <trickititit@gmail.com>
2016-09-17 20:41:09 +0300

    Горыныч добален в конфиг премиума

--- dist/gameserver/data/scripts/services/RateBonus.java
+++ dist/gameserver/data/scripts/services/RateBonus.java
@@ -19,6 +19,7 @@ import l2p.gameserver.serverpackets.MagicSkillUse;
 import l2p.gameserver.serverpackets.SystemMessage2;
 import l2p.gameserver.serverpackets.components.CustomMessage;
 import l2p.gameserver.serverpackets.components.SystemMsg;
+import l2p.gameserver.templates.item.ItemTemplate;
 import l2p.gameserver.utils.Log;
 
 public class RateBonus extends Functions {
@@ -127,9 +128,11 @@ public class RateBonus extends Functions {
             player.setHeroAura(true);
             player.broadcastCharInfo();
         }
-
-        player.getInventory().addItem(20970, 1);
-        player.broadcastCharInfo();
+        if (PremiumConfig.getPremConfigId(i).ALLOW_AGATION) {
+            player.getInventory().addItem(20970, 1);
+            player.sendPacket(SystemMessage2.obtainItems(20970, 1, 0));
+            player.broadcastCharInfo();
+        }
         player.getNetConnection().setBonus(i);
         player.getNetConnection().setBonusExpire(bonusExpire);
         player.stopBonusTask();
